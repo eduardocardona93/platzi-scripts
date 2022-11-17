274eed216083e807974a7dfbbfc0671e2f12287e
Eduardo Andres Cardona Morales <45516152+eduardocardona93@users.noreply.github.com>
2022-11-16 20:15:41 -0500

    comment

--- jenkins-tests/test/index.js
+++ jenkins-tests/test/index.js
@@ -1,6 +1,6 @@
 const sum = require('../');
 const assert = require('assert');
-
+// describe
 describe('Sum', () => {
   it('should handle positive numbers', () => {
     assert.equal(sum(3, 5), 8);
